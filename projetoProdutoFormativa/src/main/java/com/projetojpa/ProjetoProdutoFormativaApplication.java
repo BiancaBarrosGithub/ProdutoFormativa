9bec84eaf1a94c581e88f27edce4e30c6d03f435
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-11-16 11:38:46 -0300

    Add files via upload

---
+++ projetoProdutoFormativa/src/main/java/com/projetojpa/ProjetoProdutoFormativaApplication.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProdutoFormativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoProdutoFormativaApplication.class, args);
+	}
+
+}
